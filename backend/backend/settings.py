7b5646a3ae5d6d90f499d2176c5fd47d76da81fb
divinefavourak <divinefavourakanbi07@gmail.com>
2025-12-18 09:11:54 +0100

    feat: Initialize Django backend with core settings, email templates, and email test files.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -224,8 +224,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Email Settings
 EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
 EMAIL_HOST = os.getenv('EMAIL_HOST', default='smtp.gmail.com')
-EMAIL_PORT = os.getenv('EMAIL_PORT', default=587)
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', default=True)
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', default='465'))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'False').lower() in ('true', '1', 'yes')
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'True').lower() in ('true', '1', 'yes')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', default='noreply@r63teens.com')
